643bdc55441a125cb29fe6f1678c935a2e247b35
ila <ilamuhil@gmail.com>
2022-05-17 11:13:37 +0530

    Changed main branch

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,5 @@
 export default function handler(req, res) {
-	res.status(200).json({ text: 'Hellooo' });
+	res
+		.status(200)
+		.json({ text: 'Hellooo', someMoreText: 'Changes made to main branch' });
 }
